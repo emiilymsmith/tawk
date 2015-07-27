1e5cb9131d0e44b8f50db26486cf818283165146
Demo User <demouser@MacBook-Pro.local>
2015-07-27 15:08:40 -0700

    Login Handler on main.py

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import webapp2
 import jinja2
+from google.appengine.api import users
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('template'))
-
-# '/login'
+"""
+class Users(ndb.Model):
+    username = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
+"""
+# '/login' page
 class LoginHandler(webapp2.RequestHandler):
-    def get(self):
+    def get(self): #copied from learn.co
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
+
+        self.response.out.write('<html><body>%s</body></html>' % greeting)
         template = env.get_template('login.html')
         variables = {}
         self.response.write(template.render(variables))
+
 """
 # '/profile'
 class ProfileHandler(webapp2.RequestHandler):
