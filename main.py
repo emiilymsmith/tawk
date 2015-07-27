3c9f796046ccbb0da5062ef1323ff98dfb0c82b8
Demo User <demouser@MacBook-Pro.local>
2015-07-27 11:53:16 -0700

    changed ds store

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import webapp2
 
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = env.get_template('home.html')
+        template = env.get_template('main.html')
         variables = {}
         self.response.write(template.render(variables))
         #self.response.write('Hello world!')
