e70604601744ec2d9cd7c7b7b85e5c80fd5e6d60
Demo User <demouser@MacBook-Pro.local>
2015-07-28 10:42:18 -0700

    Fixed nav again

---
+++ static/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function() {
+   jQuery('.toggle-nav').click(function(e) {
+       jQuery(this).toggleClass('active');
+       jQuery('.menu ul').toggleClass('active');
+
+       e.preventDefault();
+   });
+});
+
+/* Anything that gets to the document
+   will hide the dropdown */
+$(document).click(function(){
+  $(".menu ul").addClass('active');
+});
+
+/* Clicks within the dropdown won't make
+   it past the dropdown itself */
+$(".menu").click(function(e){
+  e.stopPropagation();
+});
